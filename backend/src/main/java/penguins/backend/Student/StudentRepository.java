15563ba51332f48abb62b3b53606c7a758d6a878
Arman <129213877+arman-2200356808@users.noreply.github.com>
2023-04-16 21:06:56 +0300

    Implement with ArrayList

--- backend/src/main/java/penguins/backend/Student/StudentRepository.java
+++ backend/src/main/java/penguins/backend/Student/StudentRepository.java
@@ -1,9 +1,30 @@
 package penguins.backend.Student;
 
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-public interface StudentRepository {
-    Optional<Student> findById(long id);
+@Component
+public class StudentRepository {
 
-    Student save(Student student);
+    private final List<Student> students = new ArrayList<>();
+
+    public List<Student> findAll() {
+        return this.students;
+    }
+
+    public Optional<Student> findById(long userId) {
+        for (Student student : students)
+            if (student.getUserId() == userId)
+                return Optional.of(student);
+        return Optional.empty();
+    }
+
+    public Student save(Student student) {
+        students.remove(student);
+        students.add(student);
+        return student;
+    }
 }
