2ae3c5c8060f69a3c8dbf9001c7f141834a4cb05
Arman <129213877+arman-2200356808@users.noreply.github.com>
2023-04-16 21:04:55 +0300

    Delete CourseRegistrationException.java

--- backend/src/main/java/penguins/backend/Semester/Exception/CourseRegistrationException.java
+++
@@ -1,7 +0,0 @@
-package penguins.backend.Semester.Exception;
-
-public class CourseRegistrationException extends RuntimeException {
-    public CourseRegistrationException(String message) {
-        super(message);
-    }
-}
