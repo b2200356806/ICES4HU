2f26ad680a99a4ef02fefe9cd45ee83d5383cabd
b2200356060 <72974598+b2200356060@users.noreply.github.com>
2023-04-22 17:56:39 +0300

    Update AppController.java

--- backend/src/main/java/penguins/backend/AppController.java
+++ backend/src/main/java/penguins/backend/AppController.java
@@ -1,9 +1,9 @@
 package penguins.backend;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AppController {
     @GetMapping("/admin/manageSemester")
     public String viewHomePage() {return "admin/manageSemester";}
