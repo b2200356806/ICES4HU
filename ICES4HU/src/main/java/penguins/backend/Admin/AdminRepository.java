19e7d6944310b2bd4955a02fa29871150c9608ba
Arman <129213877+arman-2200356808@users.noreply.github.com>
2023-05-14 15:15:10 +0300

    Create AdminRepository with ArrayList

---
+++ ICES4HU/src/main/java/penguins/backend/Admin/AdminRepository.java
@@ -0,0 +1,32 @@
+package penguins.backend.Admin;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class AdminRepository {
+    private final List<Admin> admins = new ArrayList<>();
+
+    public Optional<Admin> findByUserId(long userId) {
+        for (Admin admin : admins) {
+            if (admin.getUserId() == userId) {
+                return Optional.of(admin);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public final List<Admin> findAll() {
+        return new ArrayList<>(admins);
+    }
+
+    public Admin save(Admin admin) {
+        admins.remove(admin);
+        admins.add(admin);
+        return admin;
+    }
+
+}
