94706bff409940764240437dd60be3474e404cef
Arman <129213877+arman-2200356808@users.noreply.github.com>
2023-05-14 17:22:16 +0300

    Update StudentController for new use cases

--- ICES4HU/src/main/java/penguins/backend/Student/StudentController.java
+++ ICES4HU/src/main/java/penguins/backend/Student/StudentController.java
@@ -3,19 +3,24 @@ package penguins.backend.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import penguins.backend.Course.Exception.CourseNotFoundException;
-import penguins.backend.Semester.Exception.SemesterNotStartedException;
-import penguins.backend.Semester.Exception.SemesterOngoingException;
-import penguins.backend.User.Exception.UserNotFoundException;
+import penguins.backend.Course.Course;
+import penguins.backend.Course.CourseException.CourseNotFoundException;
+import penguins.backend.Evaluation.EvaluationException.CourseAlreadyEvaluatedException;
+import penguins.backend.Evaluation.EvaluationForm.EvaluationForm;
+import penguins.backend.Evaluation.EvaluationResponse.EvaluationResponseDto;
+import penguins.backend.Semester.SemesterException.EvaluationNotStartedException;
+import penguins.backend.Semester.SemesterException.SemesterNotStartedException;
+import penguins.backend.Semester.SemesterException.AddOrDropFinishedException;
+import penguins.backend.User.UserException.UserNotFoundException;
+import penguins.backend.User.UserUpdateRequest;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/students")
+@RequestMapping("/api/students/{userId}")
 public class StudentController {
-
 
     private final StudentService studentService;
 
@@ -38,12 +43,13 @@ public class StudentController {
         return ResponseEntity.ok(output);
     }
 
+
     /**
      * Finds the student with the given id.
      * @param userId user id of the student
      * @return ResponseEntity containing the StudentDto with the given id if it exists
      */
-    @GetMapping(path = "/{userId}")
+    @GetMapping(path = "")
     public ResponseEntity<?> getStudentByUserId(@PathVariable long userId) {
         try {
             Student student = studentService.getStudentById(userId);
@@ -60,7 +66,7 @@ public class StudentController {
      * @param userId user id of the student
      * @return ResponseEntity containing a list of courses taken by the student with the given id
      */
-    @GetMapping(path = "/{userId}/courses")
+    @GetMapping(path = "/courses")
     public ResponseEntity<?> getStudentCourses(@PathVariable long userId) {
         try {
             return ResponseEntity.ok(studentService.getStudentCourses(userId));
@@ -74,20 +80,17 @@ public class StudentController {
      * Enrolls the student to the course with the given id.
      * @param userId user id of the student
      * @param courseCode course code
-     * @return ResponseEntity containing a StudentDto with updated info
+     * @return ResponseEntity containing the updated list of courses
      */
-    @PostMapping(path = "/{userId}/enroll/{courseCode}")
+    @PostMapping(path = "/courses/enroll/{courseCode}")
     public ResponseEntity<?> enroll(@PathVariable long userId, @PathVariable String courseCode) {
         try {
-            Student student = studentService.enroll(userId, courseCode);
-            StudentDto studentDto = studentToStudentDto(student);
-            return ResponseEntity.ok(studentDto);
-        } catch (UserNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found");
-        } catch (CourseNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found");
+            List<Course> courses = studentService.enroll(userId, courseCode);
+            return ResponseEntity.ok(courses);
+        } catch (UserNotFoundException | CourseNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (SemesterNotStartedException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Semester is not started yet");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
@@ -96,20 +99,75 @@ public class StudentController {
      * Drops the student from a course with the given id.
      * @param userId user id of the student
      * @param courseCode course code
-     * @return ResponseEntity containing a StudentDto with updated info
+     * @return ResponseEntity containing the updated list of courses
      */
-    @PostMapping(path = "/{userId}/drop/{courseCode}")
+    @PostMapping(path = "/courses/drop/{courseCode}")
     public ResponseEntity<?> drop(@PathVariable long userId, @PathVariable String courseCode) {
         try {
-            Student student = studentService.drop(userId, courseCode);
+            List<Course> courses = studentService.drop(userId, courseCode);
+            return ResponseEntity.ok(courses);
+        } catch (UserNotFoundException | CourseNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (AddOrDropFinishedException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Gets evaluationForms that are not null for the courses taken by the student.
+     * @param userId student user id
+     * @return EvaluationForms for the courses of the student
+     */
+    @GetMapping(path = "/evaluation")
+    public ResponseEntity<?> getEvaluationForms(@PathVariable long userId) {
+        try {
+            List<EvaluationForm> output = studentService.getEvaluationForms(userId);
+            return ResponseEntity.ok(output);
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (EvaluationNotStartedException | CourseAlreadyEvaluatedException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Evaluates a course.
+     * @param userId student user id
+     * @param courseCode course code
+     * @param responses Responses given by the student
+     */
+    @PostMapping(path = "/evaluation/{courseCode}")
+    public ResponseEntity<String> evaluate(@PathVariable long userId, @PathVariable String courseCode,
+                                           @RequestBody List<EvaluationResponseDto> responses) {
+
+        try {
+            studentService.evaluate(userId, courseCode, responses);
+            return ResponseEntity.ok("Course evaluated successfully");
+        } catch (UserNotFoundException | CourseNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (CourseAlreadyEvaluatedException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
+    }
+
+
+    /**
+     * Updates the attributes of the user
+     * @param userId student user id
+     * @param userUpdateRequest updated user attributes
+     * @return updated student
+     */
+    @PostMapping(path = "/update-info")
+    public ResponseEntity<?> updateStudent(@PathVariable long userId, @RequestBody UserUpdateRequest userUpdateRequest) {
+        try {
+            Student student = studentService.updateStudent(userId, userUpdateRequest);
             StudentDto studentDto = studentToStudentDto(student);
             return ResponseEntity.ok(studentDto);
         } catch (UserNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found");
-        } catch (CourseNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found");
-        } catch (SemesterOngoingException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Semester is still ongoing");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 
