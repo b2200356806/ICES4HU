16d3587d68db694e4973f41c7a4645ec78280773
Arman <129213877+arman-2200356808@users.noreply.github.com>
2023-05-14 15:12:10 +0300

    Add methods for new use cases

--- ICES4HU/src/main/java/penguins/backend/Admin/AdminController.java
+++ ICES4HU/src/main/java/penguins/backend/Admin/AdminController.java
@@ -4,10 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import penguins.backend.Course.Course;
-import penguins.backend.Course.Exception.CourseAlreadyExistsException;
+import penguins.backend.Course.CourseException.CourseAlreadyExistsException;
+import penguins.backend.Course.CourseException.CourseNotFoundException;
+import penguins.backend.Semester.SemesterDto;
+import penguins.backend.User.UserException.UserNotFoundException;
+import penguins.backend.User.UserUpdateRequest;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/api/admin/{UserId}")
 public class AdminController {
 
     private final AdminService adminService;
@@ -15,6 +21,17 @@ public class AdminController {
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
     }
+
+    /**
+     * Returns a list of courses in the database.
+     * @return ResponseEntity containing a list of courses
+     */
+    @GetMapping(path = "/courses")
+    public ResponseEntity<List<Course>> getCourses() {
+        List<Course> courses = adminService.getCourses();
+        return ResponseEntity.ok(courses);
+    }
+
 
     /**
      * Adds a course to the database.
@@ -28,6 +45,22 @@ public class AdminController {
             return ResponseEntity.status(HttpStatus.CREATED).body(createdCourse);
         } catch (CourseAlreadyExistsException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Course already exists");
+        }
+    }
+
+
+    /**
+     * Removes a course from the database.
+     * @param courseCode the course code of the course to remove from the database
+     * @return ResponseEntity indicating if the course was removed
+     */
+    @PostMapping(path = "/courses/remove")
+    public ResponseEntity<String> removeCourse(@RequestBody String courseCode) {
+        try {
+            adminService.removeCourse(courseCode);
+            return ResponseEntity.ok("Course removed successfully");
+        } catch (CourseNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course does not exist");
         }
     }
 
@@ -55,14 +88,118 @@ public class AdminController {
 
 
     /**
-     * Indicates whether the semester is started.
-     * @return ResponseEntity containing true if the semester is started, false otherwise
+     * Starts the evaluation.
+     * @return ResponseEntity containing HttpStatus OK
      */
-    @GetMapping(path = "/semester/status")
-    public ResponseEntity<Boolean> getSemesterStatus() {
-        return ResponseEntity.ok(adminService.getSemesterStatus());
+    @PostMapping(path = "/evaluation/start")
+    public ResponseEntity<String> startEvaluation() {
+        adminService.startEvaluation(true);
+        return ResponseEntity.ok("Evaluation started successfully");
     }
 
+
+    /**
+     * Finishes the evaluation.
+     * @return ResponseEntity containing HttpStatus OK
+     */
+    @PostMapping(path = "/evaluation/finish")
+    public ResponseEntity<String> finishEvaluation() {
+        adminService.startEvaluation(false);
+        return ResponseEntity.ok("Evaluation finished successfully");
+    }
+
+
+
+    /**
+     * Starts add or drop.
+     * @return ResponseEntity containing HttpStatus OK
+     */
+    @PostMapping(path = "/add-or-drop/start")
+    public ResponseEntity<String> startAddOrDrop() {
+        adminService.startAddOrDrop(true);
+        return ResponseEntity.ok("Add/Drop started successfully");
+    }
+
+
+    /**
+     * Finishes add or drop.
+     * @return ResponseEntity containing HttpStatus OK
+     */
+    @PostMapping(path = "/add-or-drop/finish")
+    public ResponseEntity<String> finishAddOrDrop() {
+        adminService.startAddOrDrop(false);
+        return ResponseEntity.ok("Add/Drop finished successfully");
+    }
+
+
+
+    /**
+     * Indicates whether the semester is started.
+     * @return ResponseEntity containing the semesterDto
+     */
+    @GetMapping(path = "/semester")
+    public ResponseEntity<SemesterDto> getSemester() {
+        return ResponseEntity.ok(adminService.getSemester());
+    }
+
+
+    /**
+     * Updates the attributes of the user
+     * @param userId admin user id
+     * @param userUpdateRequest updated user attributes
+     * @return updated admin
+     */
+    @PostMapping(path = "/update-info")
+    public ResponseEntity<?> updateAdmin(@PathVariable long userId, @RequestBody UserUpdateRequest userUpdateRequest) {
+        try {
+            Admin admin = adminService.updateAdmin(userId, userUpdateRequest);
+            AdminDto adminDto = adminToAdminDto(admin);
+            return ResponseEntity.ok(adminDto);
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Admits the student.
+     * @param studentId student user id
+     * @return updated student
+     */
+    @PostMapping("/admit/{studentId}")
+    public ResponseEntity<Boolean> admitStudent(long studentId) {
+        try {
+            Boolean output = adminService.admitStudent(studentId, true);
+            return ResponseEntity.ok(output);
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+
+    /**
+     * Creates the first admin object
+     * @return ResponseEntity containing true if new object created, false otherwise
+     */
+    @PostMapping("/create-first-admin")
+    public ResponseEntity<Boolean> createFirstAdmin() {
+        boolean output = adminService.createFirstAdmin();
+        if (output) {
+            return ResponseEntity.ok(true);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(false);
+        }
+    }
+
+
+    private AdminDto adminToAdminDto(Admin admin) {
+        AdminDto adminDto = new AdminDto();
+        adminDto.setFirstName(admin.getFirstName());
+        adminDto.setLastName(admin.getLastName());
+        adminDto.setUsername(admin.getUsername());
+        adminDto.setUserId(admin.getUserId());
+        return adminDto;
+    }
 
 
     @PostMapping("/add-examples")
