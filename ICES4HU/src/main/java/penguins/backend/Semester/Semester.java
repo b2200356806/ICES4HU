d6cf64d442c4739c17cd0eac28578d96c4cf8303
Arman <129213877+arman-2200356808@users.noreply.github.com>
2023-05-14 17:14:36 +0300

    Add attributes for evaluation and add/drop

--- ICES4HU/src/main/java/penguins/backend/Semester/Semester.java
+++ ICES4HU/src/main/java/penguins/backend/Semester/Semester.java
@@ -2,15 +2,16 @@ package penguins.backend.Semester;
 
 public class Semester {
 
-    private static boolean started = false;
-    private Semester() {}
+    private static boolean semesterStarted = false;
+    private static boolean evaluationStarted = false;
+    private static boolean addOrDropStarted = false;
 
     /**
      * Indicates whether the semester is started.
      * @return true if the semester is started, false otherwise
      */
-    public static boolean isStarted() {
-        return started;
+    public static boolean isSemesterStarted() {
+        return semesterStarted;
     }
 
 
@@ -18,10 +19,45 @@ public class Semester {
      * Starts and finishes the semester.
      * @param started true to start the semester, false otherwise
      */
-    public static void setStarted(boolean started) {
-        Semester.started = started;
+    public static void setSemesterStarted(boolean started) {
+        Semester.semesterStarted = started;
     }
 
+
+    /**
+     * Indicates whether the evaluation is started.
+     * @return true if the semester is started, false otherwise
+     */
+    public static boolean isEvaluationStarted() {
+        return evaluationStarted && semesterStarted;
+    }
+
+
+    /**
+     * Starts and finishes the evaluation.
+     * @param started true to start the semester, false otherwise
+     */
+    public static void setEvaluationStarted(boolean started) {
+        Semester.evaluationStarted = started;
+    }
+
+
+    /**
+     * Indicates whether the add or drop is started.
+     * @return true if the add or drop is started, false otherwise
+     */
+    public static boolean isAddOrDropStarted() {
+        return addOrDropStarted;
+    }
+
+
+    /**
+     * Starts and finishes the add or drop.
+     * @param started true to start the add or drop, false otherwise
+     */
+    public static void setAddOrDropStarted(boolean started) {
+        Semester.addOrDropStarted = started;
+    }
 
 
 }
