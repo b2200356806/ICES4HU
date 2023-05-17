30308554a270a0a159fcf3166ef07d866147e969
Arman <129213877+arman-2200356808@users.noreply.github.com>
2023-05-17 17:33:06 +0300

    Add findByName  method

--- ICES4HU/src/main/java/penguins/backend/Department/DepartmentRepository.java
+++ ICES4HU/src/main/java/penguins/backend/Department/DepartmentRepository.java
@@ -4,10 +4,22 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class DepartmentRepository {
     private final List<Department> departments = new ArrayList<>();
+
+
+    public Optional<Department> findByName(String name) {
+        for (Department department : departments) {
+            if (department.getName().equals(name)) {
+                return Optional.of(department);
+            }
+        }
+        return Optional.empty();
+    }
+
 
     public Department save(Department department) {
         departments.remove(department);
