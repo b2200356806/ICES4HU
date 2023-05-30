2e149bd3a1fbdde808bd4a6180944bec19488678
b2200356060 <72974598+b2200356060@users.noreply.github.com>
2023-05-30 23:42:42 +0300

    Updated semester management for Admin

--- ICES4HU/src/main/resources/static/admin/js/manageSemester.js
+++ ICES4HU/src/main/resources/static/admin/js/manageSemester.js
@@ -1,3 +1,12 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var scrollpos = localStorage.getItem('scrollpos');
+    if (scrollpos) window.scrollTo(0, scrollpos);
+});
+
+
+window.onbeforeunload = function (e) {
+    localStorage.setItem('scrollpos', window.scrollY);
+};
 
 const dbParam = JSON.stringify();
 const xmlhttp = new XMLHttpRequest();
@@ -22,24 +31,21 @@ xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 xmlhttp.send(dbParam);
 
 //Semester Start Button
-document.getElementById("start-btn").addEventListener("click", function()
-{
+document.getElementById("start-btn").addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/api/admin/1/semester/start");
     xhr.send();
 });
 
 //Semester Update Button
-document.getElementById("update-btn").addEventListener("click", function()
-{
+document.getElementById("update-btn").addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/api/admin/1/semester/start");
     xhr.send();
 });
 
 //Semester End Button
-document.getElementById("end-btn").addEventListener("click", function()
-{
+document.getElementById("end-btn").addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/api/admin/1/semester/finish");
     xhr.send();
@@ -47,24 +53,21 @@ document.getElementById("end-btn").addEventListener("click", function()
 
 
 //AddDrop Start Button
-document.getElementById("ad-start").addEventListener("click", function()
-{
+document.getElementById("ad-start").addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/api/admin/1/add-or-drop/start");
     xhr.send();
 });
 
 //AddDrop Update Button
-document.getElementById("ad-update").addEventListener("click", function()
-{
+document.getElementById("ad-update").addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/api/admin/1/add-or-drop/start");
     xhr.send();
 });
 
 //AddDrop End Button
-document.getElementById("ad-end").addEventListener("click", function()
-{
+document.getElementById("ad-end").addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/api/admin/1/add-or-drop/finish");
     xhr.send();
@@ -72,45 +75,41 @@ document.getElementById("ad-end").addEventListener("click", function()
 
 
 //Evaluation Start Button
-document.getElementById("eval-start").addEventListener("click", function()
-{
+document.getElementById("eval-start").addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/api/admin/1/evaluation/start");
     xhr.send();
 });
 
 //Evaluation Update Button
-document.getElementById("eval-update").addEventListener("click", function()
-{
+document.getElementById("eval-update").addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/api/admin/1/evaluation/start");
     xhr.send();
 });
 
 //Evaluation End Button
-document.getElementById("eval-end").addEventListener("click", function()
-{
+document.getElementById("eval-end").addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8080/api/admin/1/evaluation/finish");
     xhr.send();
 });
 
 
-
 //Datepicker
 startDate.min = new Date().toISOString().split("T")[0];
 addDropStartDate.min = new Date().toISOString().split("T")[0];
 evalStartDate.min = new Date().toISOString().split("T")[0];
-document.getElementById("startDate").addEventListener("change", function() {
+document.getElementById("startDate").addEventListener("change", function () {
     var input = this.value;
-    endDate.min=input
+    endDate.min = input
 });
-document.getElementById("addDropStartDate").addEventListener("change", function() {
+document.getElementById("addDropStartDate").addEventListener("change", function () {
     var input = this.value;
-    addDropEndDate.min=input
+    addDropEndDate.min = input
 });
-document.getElementById("evalStartDate").addEventListener("change", function() {
+document.getElementById("evalStartDate").addEventListener("change", function () {
     var input = this.value;
-    evalEndDate.min=input
+    evalEndDate.min = input
 });
 
