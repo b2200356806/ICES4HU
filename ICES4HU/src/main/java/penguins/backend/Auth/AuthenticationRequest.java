76fd64e4be7fa1a422eb981f15530a30701ef518
b2200356060 <72974598+b2200356060@users.noreply.github.com>
2023-05-31 21:41:16 +0300

    Implemented Authentication

---
+++ ICES4HU/src/main/java/penguins/backend/Auth/AuthenticationRequest.java
@@ -0,0 +1,17 @@
+package penguins.backend.Auth;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    private String username;
+    String password;
+}
